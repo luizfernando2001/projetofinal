39b5700d479caf964a3263ea8c93c8abb1cee895
luizfernando2001 <luiz83766@gmail.com>
2020-11-10 14:33:13 -0300

    Arquivos base

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Final</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="app\assets\css\bootstrap.min.css"> 
+    <!-- Outros -->
+    <link rel="stylesheet" href="">
+    <!-- Icomoon -->
+    <link rel="stylesheet" href="app\assets\icomoon\style.css">
+     <!-- css -->
+    <link rel="stylesheet" href="app\assets\css\meucss.css">
 </head>
 <body>
     
+<!-- Header -->
+<!-- Fim header -->
+
+
+<!-- Paginas do meu site -->
+<!-- Fim paginas do meu site -->
+
+
+<!-- Rodapé -->
+
+<script src="app\assets\js\jquery.js"></script>
+<script src="app\assets\css\bootstrap.min.css"></script>
+<script src=""></script>
 </body>
 </html>
